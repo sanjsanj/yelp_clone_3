aa307cda3ec25b4dedb294614a0b1de9f6040ed7
James Miller <james.fl.miller@live.co.uk>
2015-05-06 08:51:53 +0100

    Add validations to review and restaurant

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,4 +4,9 @@ describe Review, type: :model do
 
   it { is_expected.to belong_to :restaurant }
 
+  it 'is invalid if the rating is more than 5' do
+    review = Review.new(rating: 10)
+    expect(review).to have(1).error_on(:rating)
+  end
+
 end
