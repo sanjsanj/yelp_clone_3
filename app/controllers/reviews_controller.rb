faba925358f6845f174736fe5bccec194b719379
Kanishk Walia <kanishk.walia@gmail.com>
2015-05-07 17:25:14 +0100

    Added deleting reviews and user specificity functionality

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -15,4 +15,22 @@ class ReviewsController < ApplicationController
     combined_params['user_id'] = current_user.id
     combined_params
   end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    flash[:notice] = 'Review deleted successfully'
+    redirect_to restaurants_path
+  end
+
+  def verify_creator(review)
+    unless created_by_current_user(review)
+      flash[:notice] = "You may only delete a review that you added"
+      redirect_to restaurants_path
+    end
+  end
+
+  def created_by_current_user(review)
+    review.user_id == current_user.id
+  end
 end
